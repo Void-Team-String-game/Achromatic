b0079ff25958c5592636fc905ebd786890cc0b0f
Balloon208 <gnswldms2@gmail.com>
2023-08-17 20:29:01 +0900

    mAI
    몸 ai 대충

--- Achromatic/Assets/1_Scripts/Player/Mob.cs
+++ Achromatic/Assets/1_Scripts/Player/Mob.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI;
+
 public class Mob : MonoBehaviour
 {
+    NavMeshAgent nav;
+    public Transform target;
+
     public float health = 100f;
 
+    private void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        nav.SetDestination(target.transform.position);
+    }
     public void TakeDamage(float amount)
     {
         health -= amount;
@@ -20,3 +34,4 @@ public class Mob : MonoBehaviour
         Destroy(gameObject);
     }
 }
+
