15e63c05e29cbc147c40e110c8a143ead1085bb7
황현민 <hwanghyunmin0650@gmail.com>
2023-09-21 18:01:18 +0900

    MainMenu
    메인메뉴

--- Achromatic/Assets/1_Scripts/UI/PauseMenu.cs
+++ Achromatic/Assets/1_Scripts/UI/PauseMenu.cs
@@ -47,7 +47,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Time.timeScale = 1f;
-        //SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void QuitMenu()
