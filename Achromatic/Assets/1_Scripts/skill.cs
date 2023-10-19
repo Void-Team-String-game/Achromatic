ec29a0bbdf905755327f726abeec11cc1242fc80
subak222 <sbag88686@gmail.com>
2023-10-19 19:07:19 +0900

    Healing

---
+++ Achromatic/Assets/1_Scripts/skill.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+
+public class skill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+    private Status status;
+    HpScript hpScript;
+    public VisualEffect VEA;
+
+
+    public float timer;
+    private float time;
+    void Start()
+    {
+        time = timer;
+        hpScript = GameObject.Find("Hpbar").GetComponent<HpScript>();
+        player = GameObject.FindWithTag("player");
+        status = player.GetComponent<Status>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                VisualEffect visualEffectInstance = Instantiate(VEA,new Vector3(transform.position.x,transform.position.y+0.1f-1,transform.position.z),transform.rotation);
+                visualEffectInstance.Play();
+                Debug.Log("±è¹Î¿ì ³»½Ã");
+                if (status.hp < status.Maxhp)
+                {
+                    time = timer;
+                    status.hp += 5;
+                    hpScript.UpdateHp(status.hp);
+                }
+            }
+
+        }
+    }
+}
